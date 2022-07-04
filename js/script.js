d81f2554b54ae1097d56b05b55db81039223c21e
unknown <di1025p@gmail.com>
2022-07-04 17:54:01 +0900

    k-water 작업중

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-$(document).ready(function () {
-});
-window.onload = function () {};
+$(document).ready(function () {});
+window.onload = function () {
+  // 메뉴 기능
+  let header = $('.header');
+  let gnb = $('.gnb');
+  let gnbH = gnb.height();
+
+  gnb.mouseenter(function () {
+    header.css('height', gnbH);
+  });
+  gnb.mouseleave(function () {
+    header.css('height', 70);
+  });
+
+  // 비주얼 슬라이드
+  new Swiper('.sw-visual', {
+    slidesPerView: 8,
+    loop: true,
+    navigation: {
+      prevEl: '.sw-visual-prev',
+      nextEl: '.sw-visual-next'
+    }
+  });
+};
